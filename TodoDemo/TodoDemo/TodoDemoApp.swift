9e558fe9d2eade07f131cd27dc735723f1d6cceb
simplybenjmain2.0@gmail.com <simplybenjamin2.0@gmail.com>
2022-01-19 16:53:42 +0100

    update AppIcon

---
+++ TodoDemo/TodoDemo/TodoDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoDemoApp.swift
+//  TodoDemo
+//
+//  Created by Benjamin Akhigbe on 19/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
